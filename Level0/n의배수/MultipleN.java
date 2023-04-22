b132a453c5f85e25eff8632664481a7149c794f3
Shanyu Li <uhaha529@gmail.com>
2023-04-23 00:13:51 +0900

    docs: level0  부분 완성

---
+++ Level0/n의배수/MultipleN.java
@@ -0,0 +1,20 @@
+package Level0.n의배수;
+// 정수 num과 n이 매개 변수로 주어질 때,
+// num이 n의 배수이면 1을 return n의 배수가 아니라면
+// 0을 return하도록 solution 함수를 완성해주세요.
+public class MultipleN {
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        int num = 98;
+        int n = 2;
+        System.out.println(s.solution(num,n));
+    }
+
+}
+class Solution {
+    public int solution(int num, int n) {
+        int answer = 0;
+        answer = num % n == 0 ? 1 : 0;
+        return answer;
+    }
+}
