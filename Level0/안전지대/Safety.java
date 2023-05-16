237f9c0e63849d4fef7936f211cdf2ccbe6dd6fc
Shanyu Li <uhaha529@gmail.com>
2023-05-16 20:15:51 +0900

    docs: 프로그래머스 level0 안전지대 : 간단하게 생각하면 해결하기 쉽다.

--- Level0/안전지대/Safety.java
+++ Level0/안전지대/Safety.java
@@ -6,8 +6,8 @@ public class Safety {
     public static void main(String[] args) {
         Solution s = new Solution();
 //        int[][] board = {{0, 0, 0, 0, 0}, {0, 0, 0, 0, 0}, {0, 0, 0, 0, 0}, {0, 0, 1, 0, 0}, {0, 0, 0, 0, 0}};
-//        int[][] board = {{0, 0, 0, 0, 0}, {0, 0, 0, 0, 0}, {0, 0, 0, 0, 0}, {0, 0, 1, 1, 0}, {0, 0, 0, 0, 0}};
-        int[][] board = {{0, 0, 0, 0, 0}, {0, 0, 0, 0, 0}, {0, 0, 0, 0, 0}, {0, 0, 0, 0, 0}, {1, 1, 1, 1, 1}};
+        int[][] board = {{0, 0, 0, 0, 0}, {0, 0, 0, 0, 0}, {0, 0, 0, 0, 0}, {0, 0, 1, 1, 0}, {0, 0, 0, 0, 0}};
+//        int[][] board = {{0, 0, 0, 0, 0}, {0, 0, 0, 0, 0}, {0, 0, 0, 0, 0}, {0, 0, 0, 0, 0}, {1, 1, 1, 1, 1}};
         System.out.println(s.solution(board));
     }
 
@@ -18,25 +18,27 @@ class Solution {
         int[][] copy = Arrays.stream(board)
                 .map(int[]::clone)
                 .toArray(int[][]::new);
+        int overX = copy.length;
+        int overY = copy[0].length;
 
         for(int i = 0; i < board.length; i++){
             for(int j = 0; j < board[i].length; j++){
                 if(board[i][j] == 1){
-                    if (i-1 > 0 && copy[i-1][j] != 1){// 위쪽
+                    if (i > 0){// 위쪽
                         copy[i-1][j] = 2;}// 위쪽
-                    if (i+1 < board.length && copy[i+1][j] != 1){
+                    if (i < overX - 1 ){
                         copy[i+1][j] = 2;} //아래쪽
-                    if (j-1 > 0 && copy[i][j-1] != 1){
+                    if (j > 0){
                         copy[i][j-1] = 2;} //왼쪽
-                    if (j+1 < board[i].length && copy[i][j+1] != 1){
+                    if (j < overY - 1){
                         copy[i][j+1] = 2;} //오른쪽
-                    if (i-1 > 0 && j-1 > 0 && copy[i-1][j-1] != 1){
+                    if (j > 0 && i > 0){
                         copy[i-1][j-1] = 2;} // 왼쪽 위쪽
-                    if (i-1 > 0 && j+1 < board[i].length && copy[i-1][j+1] != 1){
+                    if (i > 0 && j < overY - 1){
                         copy[i-1][j+1] = 2;} //오른쪽 위쪽
-                    if (i+1 < board.length && j-1 > 0 && copy[i+1][j-1] != 1){
+                    if (i < overX - 1 && j > 0) {
                         copy[i+1][j-1] = 2;}// 왼쪽 아래
-                    if (i+1 < board.length && j+1 < board[i].length && copy[i+1][j+1] != 1){
+                    if (i < overX - 1 && j < overY - 1){
                         copy[i+1][j+1] = 2;} // 오른쪽 아래
                 }
             }
