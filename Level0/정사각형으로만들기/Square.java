559071e81e446d9b05be6035e9f2b8883b36a960
Shanyu Li <uhaha529@gmail.com>
2023-05-20 23:53:00 +0900

    docs: 프로그래머스 level0 정사각형으로만들기 수정

--- Level0/정사각형으로만들기/Square.java
+++ Level0/정사각형으로만들기/Square.java
@@ -1,45 +1,29 @@
 package Level0.정사각형으로만들기;
-
-import java.util.ArrayList;
-import java.util.LinkedList;
-
 public class Square {
     public static void main(String[] args) {
         Solution s = new Solution();
         int[][] arr = {{572, 22, 37}, {287, 726, 38}, {85, 137, 292}, {487, 13, 876}};
         int[][] arr1 = {{57, 192, 534, 2},{9, 345, 192, 999}};
         int[][] arr2 = {{1, 2},{3, 4}};
-        int[][] arr3 = {{1}};
+        int[][] arr3 = {{1,2,3}};
+        int[][] arr4 = {{1},{1}};
         System.out.println(s.solution(arr));
         System.out.println(s.solution(arr1));
         System.out.println(s.solution(arr2));
         System.out.println(s.solution(arr3));
+        System.out.println(s.solution(arr4));
     }
 }
 class Solution {
-    public ArrayList<LinkedList<Integer>> solution(int[][] arr) {
-        ArrayList<LinkedList<Integer>> answer = new ArrayList<>();
-        for (int i = 0; i < arr.length; i++){
-            LinkedList<Integer> list = new LinkedList<>();
-            for (int j = 0; j < arr[i].length; j++){
-                list.add(arr[i][j]);
-            }
-            answer.add(list);
+    public int[][] solution(int[][] arr) {
+        if(arr.length==arr[0].length){
+            return arr;
         }
-
-        if(arr[0].length == arr.length){
-            return answer;
-        } else if(arr[0].length < arr.length) {
-            for (int z = 0; z < arr.length; z++){
-                answer.get(z).add(0);
-            }
-        } else if (arr[0].length > arr.length) {
-            for (int z = arr.length; z < arr[0].length; z++){
-                LinkedList<Integer> list = new LinkedList<>();
-                for (int i = 0; i < arr[0].length; i++){
-                    list.add(0);
-                }
-                answer.add(list);
+        int answerSize = Math.max(arr.length, arr[0].length);
+        int[][] answer = new int[answerSize][answerSize];
+        for(int x=0;x<arr.length;x++){
+            for(int y=0;y<arr[0].length;y++){
+                answer[x][y]=arr[x][y];
             }
 
         }
